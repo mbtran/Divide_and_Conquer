22d11eb928cf09a3580a8462218808eb7b27a031
Mike Tran <michaelbuutran@gmail.com>
2018-05-07 18:29:16 -0600

    Quick edit to comments

--- Merge Sort.py
+++ Merge Sort.py
@@ -1,14 +1,13 @@
+# Lists to test Merge Sort
 unsorted = [1, 2, 5, 12, 23, 6, 21, 15, 73, 24, 45, 86, 4, 58, 78,
             77, 63, 43, 45, 32, 122, 32, 21, 23, 55, 32, 76, 75, 22,
             41, 22, 66, 77, 88]
-
 shortList = [3, 12, 32, 45, 23, 12, 63, 33]
-
 unsorted1 = [60, 20, 32, 45, 23, 13, 63, 33, 40, 99, 2, 25, 109, 44, 200, 1, 65]
 
 
 def MergeSort(unsortedlist):
-    '''Recursive function to sort list.'''
+    """Recursive function to sort list."""
     if int(len(unsortedlist)) < 2:
         return unsortedlist
     else:
@@ -20,6 +19,7 @@ def MergeSort(unsortedlist):
 
 
 def Merge(left, right):
+    """Merging the split numbers"""
     print(left)
     print(right)
     i = 0
